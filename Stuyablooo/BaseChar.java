6eec2f7c9983fa6492bcd67292d126779e5663f5
kaytrean <kaytrean@gmail.com>
2014-10-22 02:21:57 -0400

    Update BaseChar.java

--- Stuyablooo/BaseChar.java
+++ Stuyablooo/BaseChar.java
@@ -9,11 +9,11 @@ public class BaseChar{
     public BaseChar(int h, int m, int a, int d, int s, int ac, int c, String n, String w){
 	hp=h;
 	mp=m;
-	atk=a;
-	def=d;
-	spd=s;
-	acc=ac;
-	lck=c;
+	atk=r.nextInt(3)+a-1;
+	def=r.nextInt(4)+d-2;
+	spd=r.nextInt(2)+s-1;
+	acc=r.nextInt(10)+ac-5;
+	lck=r.nextInt(3)+c-1;
 	name=n;
 	weapon=w;
     }
@@ -66,11 +66,11 @@ public class BaseChar{
 	    if (other.getDef()>this.getAtk()){
 		return "" + this + " could deal no damage to " + other;
 	    }
-	    if (r.nextInt(100)+1<lck){
-		other.setHP(other.getHP()+2*(other.getDef()-this.getAtk()));
+        if (r.nextInt(100)+1<lck){
+		other.setHP( other.getHP()+r.nextInt(3)-3+2*(other.getDef()-this.getAtk()));
 		return "" + this + " attacked " + other + " with " + weapon + " and injured " + other;
 	    }
-	    other.setHP(other.getHP()+other.getDef()-this.getAtk());
+	    other.setHP(other.getHP()+r.nextInt(2)-2+other.getDef()-this.getAtk());
 	    return "" + this + " attacked " + other + " with " + weapon + " and injured " + other;
 	}
 	return "" + this + " missed " + other;
