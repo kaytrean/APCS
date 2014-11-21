e4318f42923d32cd18298cd6733ac240b93950b9
kaytrean <kaytrean@gmail.com>
2014-11-20 23:39:33 -0500

    k

--- 18_WordSearch/WordSearch.java
+++ 18_WordSearch/WordSearch.java
@@ -1,108 +1,114 @@
-import java.io.*;
-import java.util.*;
-
-public class WordSearch{
+public class WordSearch {
     private char[][] board;
-    Random rnd = new Random();
-    public WordSearch(int r,int c){
-    	board = new char[r][c];
-    	for (int i=0;i<board.length;i++){
-    	    for (int j=0;j<board[i].length;j++){
-    		    board [i][j] = '.';
-    	    }
-    	}
-    }
 
-    public WordSearch(){
-	    this(30,20);
-        }
+    public WordSearch(int r, int c){
+	board = new char[r][c];
+	for (int i = 0; i < board.length; i++) {
+	    for (int j = 0; j < board[i].length; j++) {
+		board[i][j]='.';
+	    }
+	}
+    }
+    public WordSearch() {
+	this(20,40);
+    }
+ 
     public String toString(){
-    	String s = "";
-    	for (int i=0;i<board.length;i++){
-    	    for (int j=0;j<board[i].length;j++){
-    		s = s + board[i][j] + " ";
-    	    }
-    	    s = s + "\n";
-    	}
-    	return s;
+	String s = "";
+	for (int i = 0; i < board.length; i++) {
+	    for (int j = 0; j < board[i].length; j++) {
+		s = s + board[i][j];
+	    }
+	    s = s + "\n";
+	}
+	return s;
     }
-    public void addWord(String w){
-    	int rand = rnd.nextInt(8);
-    	while (rand != 0){
-    	    rand = rnd.nextInt(8);
-    	}
-    	if (rand == 0){
-    	    addWordH1(w, rnd.nextInt(board.length), rnd.nextInt(board[0].length-w.length()));
-    	}
-    }
-    public void addWordH1(String w, int ro, int co){
-        boolean matching = true;
-        int row = ro;
-        int col = co;
-        char val;
-    	//System.out.println(w + " " + r + " " + c);
-    	for (int i=0;i<w.length();i++){
-    	    val = board[row][col];
-    	    //System.out.println(val + " " + w.charAt(i));
-    	    if (!(val=='.') && !(val==w.charAt(i))){
-    	        matching = false;
-    	        break;
-    	    }
-    	    col++;
-    	}
-    	if (matching){
-    	    for (int i=0;i<w.length();i++){
-    	        //System.out.println(w.charAt(i));
-    	        board[ro][co] = w.charAt(i);
-        	    co++;
-    	    }
-    	}
-    	else{
-    	    addWordH1(w, rnd.nextInt(board.length), rnd.nextInt(board[0].length-w.length()));
-    	}
-    }
-    public void addWordH2(String w, int r, int c){
-        boolean matching = true;
-        int col = c;
-        int row = r;
-        char val;
-        for (int i=0;i<w.length();i++){
-            val = board[row][col];
-            if (!(val=='.') && !(val==w.charAt(i))){
-                matching = false;
-                break;
-            }
-            col--;
-        }
-        if (matching){
-            for (int i=0;i<w.length();i++){
-                board[r][c] = w.charAt(i);
-                c--;
-            }
-        }
-        else{
-            addWordH2(w, rnd.nextInt(board.length), rnd.nextInt(board[0].length-w.length())+w.length());
-        }
-    }
-    public void addWordV1(String w, int r, int c){
-        for (int i=0;i<w.length();i++){
-            board[r][c] = w.charAt(i);
-            r++;
-        }
-    }
-    public void addWordV2(String w, int r, int c){
 
+    public String reverse(String w){
+	String s = "";
+	for (int i = w.length() - 1;i >= 0;i--){
+	    s += w.charAt(i);
+	}
+	return s;
     }
-    public void addWordD1(String w, int r, int c){
 
+    public boolean overlapH(String a, int row, int col){
+	int i = 0;
+	int c = col;
+	while (i < a.length()){
+	    if ((a.charAt(i) == board[row][c])||
+		(board[row][c] == '.')){
+		i++;
+		c++;
+	    }
+	    else {return false;}
+	}
+	return true;
     }
-    public void addWordD2(String w, int r, int c){
 
+    public boolean overlapV(String a, int row, int col){
+	int i = 0;
+	int r = row;
+	while (i < a.length()){
+	    if ((a.charAt(i) == board[r][col])||
+		(board[r][col] == '.')){
+		i++;
+		r++;
+	    }
+	    else {return false;}
+	}
+	return true;
     }
-    public void addWordD3(String w, int r, int c){
 
+ 
+    public void addWordH(String w, int row, int col){
+	int c = col;
+	if ((w.length() + col <= board[0].length)&&
+	    (col >= 0)&&
+	    (row >= 0 && row < board.length)){
+	    if (overlapH(w, row, col) == true){
+		for (int i=0; i < w.length();i++){
+		    board[row][c] = w.charAt(i);
+		    c++;
+		}
+	    }
+	}
     }
-    public void addWordD4(String w, int r, int c){
 
+    public void addWordHR(String w, int row, int col){
+	String s = reverse(w);
+	addWordH(s, row, col);
+    }
+
+    public void addWordV(String w, int row, int col){
+	int r = row;
+	if ((w.length() + row <= board.length)&&
+	    (row >= 0)&&
+	    (col >= 0 && col < board[0].length)){
+	    if (overlapV(w, row, col) == true){
+		for (int i = 0; i < w.length();i++){
+		    board[r][col] = w.charAt(i);
+		    r++;
+		}
+	    }
+	}
+    }
+
+    public void addWordVR(String w, int row, int col){
+	String s = reverse(w);
+	addWordV(s, row, col);
+    }
+		
+    public static void main(String[] args) {
+	WordSearch w = new WordSearch();
+	System.out.println(w);
+	w.addWordV("hello",3,15); // should work
+	w.addWordV("look",4,15); // test illegal overlap
+       	w.addWordV("look",6,15); // test legal overlap       
+	w.addWordV("look",-3,20); // test illegal row      
+	w.addWordV("look",3,55); // test illegal col
+	w.addWordVR("hello",5,10); // test reverse direction
+	w.addWordV("pool",3,10); // test legal overlap in reverse direction
+	System.out.println(w);
     }
 }
