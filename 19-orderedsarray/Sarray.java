46bcecd2871727c2c73e5bccd1e38066d792f3e6
kaytrean <kaytrean@gmail.com>
2014-12-01 08:39:28 -0500

    Update Sarray.java

--- 19-orderedsarray/Sarray.java
+++ 19-orderedsarray/Sarray.java
@@ -1,111 +1,86 @@
-import java.util.*;
-
 public class Sarray {
-    private String[] data;
-    private int last;
+    int[] data;
+    int   last;
 
     public Sarray() {
-	data = new String[10];
-	data[0] = "a";
-	data[1] = "b";
-	data[2] = "c";
-	data[3] = "d";
-	data[4] = "e";
-	data[5] = "f";
+        // set up the initial instance variables
+	data = new int[10];
 	last = 5;
     }
-    public boolean add(String s){
-        if (last == data.length-1){
-	    makeBigger(data, 20);
-	    data[last+1] = s;
-	}
 
-	else {
-	    data[last +1] = s;
+    public boolean add(int i){
+        // adds an item to the end of the list, grow if needed
+        // returns true
+	if (data.length > size()){
+	    data[size()] = i;
 	}
-	last++;
+	else{
+	    int[] newData  = new int[data.length+1];
+	    for (int c = 0; c < data.length; c++){
+		newData[c] = data[c];
+	    }
+	    newData[newData.length-1] = i;
+	    data = newData;
+	}
 	return true;
     }
-	// adds an item to the end of the list, grow if needed
-        // returns true
 
 
     public void  add(int index, int i){
-       	if (index > last + 1){
-	    throw new ArrayIndexOutOfBoundsException();
-	}
-	if (last == data.length -1){
-	    makeBigger(data, 20);
+        // adds item i  at index, shifting everything down as needed.
+        // also grows as needed
+	if (data.length > size()){
+	    for (int c = data.length; c > index; c--){
+		data[c] = data[c-1];
 	    }
-	for (int g = last; g >= index; g--){
-		data[g+1] = data[g];
-	}
 	    data[index] = i;
-	    last++;
-    }
-	   
-	    
-	// adds item i  at index, shifting everything down as needed.
-        // also grows as needed 
-
-    public int getLast(){
-	return last;
+	}
+	else{
+	    int[] newData = new int[data.length+1];
+	    for (int c = 0; c < index ; c++){
+		newData[c] = data[c];
+	    }
+	    newData[index] = i;
+	    for (int c = index + 1; c < data.length; c++) {
+		newData[c+1] = data[c];
+	    }
+	    data = newData;
+	}
     }
 
     public int size() {
-	return last + 1;
-	    
-	// returns the number of items in the list (not the array size)
-    }
-    public String[] getData(){
-	return data;
+        // returns the number of items in the list (not the array size)
+	int index = 0;
+	searchFromEnd:
+	for (int i = data.length; i>0 ; i--){
+	    if (data[i] != 0) {
+		index = i+1;
+		break searchFromEnd;
+	    }
+	}
+	return index;
     }
 
     public int get(int index) {
-        return data[index];
-	    
-        // returns the item at location index of the lsit
+        // returns the item at location index of the list
+	return data[index];
     }
 
     public int set(int index, int i){
-        int oldvalue = data[index];
+        // sets the item at location index to value i
+        // returns the old value.
+	int old = data[index];
 	data[index] = i;
-	return oldvalue;
-	    
-	// sets the item at location index to value i
-        // returns the old value. 
+	return old;
     }
 
     public int remove(int index){
-	int oldvalue = data[index];
-        for (int i = index;i<=last;i++){
+        // removes the item at index i
+        // returns the old value
+	int old = data[index];
+	for (int i = index; i < size(); i++){
 	    data[i] = data[i+1];
 	}
-	if (index == last){
-	    last--;
-	}
-	if (index > last){
-	    throw new ArrayIndexOutOfBoundsException();
-	}
-	return oldvalue;
-	// removes the item at index i
-        // returns the old value
+	return old;
     }
-    public void makeBigger(int[] before, int numSpaces){
-	int[] longer = new int[data.length + numSpaces];
-	System.arraycopy(data,0,longer,0,data.length);
-	data = new int[longer.length];
-	System.arraycopy(longer, 0, data, 0, data.length);
-    }
-  
-public String printArray(){
-	String ans = "{";
-	for (int i = 0; i < data.length; i++){
-	    ans = ans + data[i] + ",";
-	}
-	ans = ans + "}";
-	return ans;
-    }
-
-
 }
